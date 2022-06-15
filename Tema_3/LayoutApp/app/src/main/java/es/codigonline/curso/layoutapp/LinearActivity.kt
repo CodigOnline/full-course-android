8a2f2e00e50c58d0759cbb0ff79f85011149bc05
Alvaro Ortega Marmol <alortegama@outlook.com>
2022-06-15 17:57:34 +0200

    LinearLayout

---
+++ Tema_3/LayoutApp/app/src/main/java/es/codigonline/curso/layoutapp/LinearActivity.kt
@@ -0,0 +1,11 @@
+package es.codigonline.curso.layoutapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear)
+    }
+}

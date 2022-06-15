3793a2888874180ef335ba34aac6c6cd82653488
Alvaro Ortega Marmol <alortegama@outlook.com>
2022-06-15 21:23:53 +0200

    Table & Grid Layout

---
+++ Tema_3/LayoutsApp/app/src/main/java/es/codigonline/curso/layoutsapp/GridActivity.kt
@@ -0,0 +1,11 @@
+package es.codigonline.curso.layoutsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GridActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grid)
+    }
+}
---
+++ Tema_3/LayoutsApp/app/src/main/java/es/codigonline/curso/layoutsapp/TableActivity.kt
@@ -0,0 +1,11 @@
+package es.codigonline.curso.layoutsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TableActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table)
+    }
+}

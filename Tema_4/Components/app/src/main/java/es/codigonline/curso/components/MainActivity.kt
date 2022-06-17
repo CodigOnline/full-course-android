7ff23b85d1b1036411447caaf52ca5734d399962
Alvaro Ortega Marmol <alortegama@outlook.com>
2022-06-17 16:31:08 +0200

    viewBinding

--- Tema_4/Components/app/src/main/java/es/codigonline/curso/components/MainActivity.kt
+++ Tema_4/Components/app/src/main/java/es/codigonline/curso/components/MainActivity.kt
@@ -2,10 +2,22 @@ package es.codigonline.curso.components
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import es.codigonline.curso.components.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+/*        val textView = binding.tv1
+        textView.text = "Hola Mundo!!!"*/
+
+        binding.tv1.text = "Hola Mundo!!!"
+
     }
 }

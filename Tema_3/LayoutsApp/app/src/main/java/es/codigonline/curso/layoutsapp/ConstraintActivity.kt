5231ae756ed49f416e0a693c3996a2a32cb9b3e1
Alvaro Ortega Marmol <alortegama@outlook.com>
2022-06-17 12:43:54 +0200

    ConstraintLayout

---
+++ Tema_3/LayoutsApp/app/src/main/java/es/codigonline/curso/layoutsapp/ConstraintActivity.kt
@@ -0,0 +1,11 @@
+package es.codigonline.curso.layoutsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint)
+    }
+}

ed7f6d8270d8a48251e73e945738b1cda7f5ba22
Alvaro Ortega Marmol <alortegama@outlook.com>
2022-06-20 21:44:08 +0200

    TextField

--- Tema_4/Components/app/src/main/java/es/codigonline/curso/components/MainActivity.kt
+++ Tema_4/Components/app/src/main/java/es/codigonline/curso/components/MainActivity.kt
@@ -3,6 +3,11 @@ package es.codigonline.curso.components
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doBeforeTextChanged
+import com.google.android.material.textfield.TextInputEditText
 import es.codigonline.curso.components.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +18,62 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.textInputETNamePerson.setOnFocusChangeListener { editText, isFocus ->
+            if (isFocus) {
+                binding.textInputLayoutNamePerson.error = null
+                return@setOnFocusChangeListener
+            }
 
-/*        val textView = binding.tv1
-        textView.text = "Hola Mundo!!!"*/
 
-        binding.tv1.text = "Hola Mundo!!!"
+            val length = (editText as TextInputEditText).length()
+
+            //switch
+            //when
+            when {
+                length in 0..9 -> {
+                    binding.textInputLayoutNamePerson.error =
+                        "El nombre debe de contener como mínimo 10 letras"
+                }
+                length > 50 -> {
+                    binding.textInputLayoutNamePerson.error =
+                        "El nombre debe de contener entre 10 y 50 letras"
+                }
+                else -> {
+                    binding.textInputLayoutNamePerson.error = null
+                }
+            }
+        }
+
+        binding.textInputETNamePerson.doAfterTextChanged {
+            val length = it!!.length
+            if (length > 15) {
+                it.delete(15,length)
+            }
+
+        }
+
+
+        binding.textInputETNamePerson.addTextChangedListener { editText ->
+
+            editText?.let { notNullEditText ->
+                val length = notNullEditText.length
+                //switch
+                //when
+                when {
+                    length > 50 -> {
+                        binding.textInputLayoutNamePerson.error =
+                            "El nombre debe de contener entre 10 y 50 letras"
+                    }
+                    else -> {
+                        binding.textInputLayoutNamePerson.error = null
+                    }
+                }
+
+            }
+
+
+        }
+
 
     }
 }

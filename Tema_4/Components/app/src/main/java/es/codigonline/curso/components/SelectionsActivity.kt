75bc4feab90dde2fbb89e2deddafa1a7916083a1
Alvaro Ortega Marmol <alortegama@outlook.com>
2022-06-21 20:50:55 +0200

    Selectables

--- Tema_4/Components/app/src/main/java/es/codigonline/curso/components/SelectionsActivity.kt
+++ Tema_4/Components/app/src/main/java/es/codigonline/curso/components/SelectionsActivity.kt
@@ -2,10 +2,50 @@ package es.codigonline.curso.components
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import es.codigonline.curso.components.databinding.ActivitySelectionsBinding
 
 class SelectionsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySelectionsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_selections)
+        binding = ActivitySelectionsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.checkbox.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+            }
+        }
+        binding.switchMaterial.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+            }
+        }
+
+        val radio1 = binding.radio1.id
+        val radio2 = binding.radio2.id
+        val radio3 = binding.radio3.id
+
+
+        binding.radioGroup.setOnCheckedChangeListener { _, radioID ->
+
+            when (radioID) {
+                radio1 -> Toast.makeText(this@SelectionsActivity, "Hombre", Toast.LENGTH_LONG)
+                    .show()
+                radio2 -> Toast.makeText(this@SelectionsActivity, "Mujer", Toast.LENGTH_LONG).show()
+                else -> Toast.makeText(this@SelectionsActivity, "Otro", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+        //binding.radioGroup.checkedRadioButtonId
     }
 }

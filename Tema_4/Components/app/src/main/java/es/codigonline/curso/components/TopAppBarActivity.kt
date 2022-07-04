75c73c1b5b7676c9d199f4ff31c365f953c7e23e
Alvaro Ortega Marmol <info@codigonline.com>
2022-07-04 18:30:47 +0200

    Top App Bar II

--- Tema_4/Components/app/src/main/java/es/codigonline/curso/components/TopAppBarActivity.kt
+++ Tema_4/Components/app/src/main/java/es/codigonline/curso/components/TopAppBarActivity.kt
@@ -2,10 +2,52 @@ package es.codigonline.curso.components
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import es.codigonline.curso.components.databinding.ActivityTopAppBarBinding
 
 class TopAppBarActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTopAppBarBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_top_app_bar)
+        binding = ActivityTopAppBarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.topAppBar.setNavigationOnClickListener {
+            Toast.makeText(this, "Cerrar", Toast.LENGTH_SHORT).show()
+        }
+        binding.topAppBar.setOnMenuItemClickListener {
+            //switch
+            when (it.itemId) {
+                R.id.edit -> {
+                    clickEdit()
+                    true
+                }
+                R.id.favorite -> {
+                    clickFav()
+                    true
+                }
+                R.id.more -> {
+                    clickMore()
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+
+    private fun clickEdit() {
+        Toast.makeText(this, "Editar", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun clickFav() {
+        Toast.makeText(this, "Favorito", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun clickMore() {
+        Toast.makeText(this, "Más opciones...", Toast.LENGTH_SHORT).show()
+
     }
 }

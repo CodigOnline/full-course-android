b7f45c3aab28eeb9329a758ba1c3b30db356f39b
Alvaro Ortega Marmol <info@codigonline.com>
2022-07-04 18:07:48 +0200

    Action Bar I

---
+++ Tema_4/Components/app/src/main/java/es/codigonline/curso/components/TopAppBarActivity.kt
@@ -0,0 +1,11 @@
+package es.codigonline.curso.components
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TopAppBarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top_app_bar)
+    }
+}

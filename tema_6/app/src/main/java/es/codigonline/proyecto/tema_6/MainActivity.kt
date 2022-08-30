8d005c3c009740358b4ce99a514548f52bd78b17
Alvaro Ortega Marmol <info@codigonline.com>
2022-08-30 19:00:33 +0200

    Replace Fragment

--- tema_6/app/src/main/java/es/codigonline/proyecto/tema_6/MainActivity.kt
+++ tema_6/app/src/main/java/es/codigonline/proyecto/tema_6/MainActivity.kt
@@ -2,24 +2,40 @@ package es.codigonline.proyecto.tema_6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 import es.codigonline.proyecto.tema_6.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var fragmentManager: FragmentManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        fragmentManager = supportFragmentManager
+
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(binding.fragment.id, UsuarioFragment(), "usuarioFragment")
+        transaction.commit()
+
     }
 
     override fun onStart() {
         super.onStart()
         binding.addFragment.setOnClickListener {
 
-            val supportFragmentManager = supportFragmentManager
-            val transaction = supportFragmentManager.beginTransaction()
-            transaction.add(binding.llFragments.id, UsuarioFragment())
-            transaction.commit()
+            val blankFragment =
+                fragmentManager.findFragmentByTag("blankFragment") as BlankFragment?
+            blankFragment?.let { //if
+                val transaction = fragmentManager.beginTransaction()
+                transaction.replace(binding.fragment.id, UsuarioFragment(), "usuarioFragment")
+                transaction.commit()
+            } ?: run { //else
+                val transaction = fragmentManager.beginTransaction()
+                transaction.replace(binding.fragment.id, BlankFragment(), "blankFragment")
+                transaction.commit()
+            }
+
         }
     }
 }

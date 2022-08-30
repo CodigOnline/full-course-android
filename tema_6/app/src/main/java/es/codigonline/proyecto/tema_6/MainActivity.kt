3c0dace19ce257a7d90a9badb5baef58a11477b9
Alvaro Ortega Marmol <info@codigonline.com>
2022-08-30 13:48:21 +0200

    ViewBinding Fragment

--- tema_6/app/src/main/java/es/codigonline/proyecto/tema_6/MainActivity.kt
+++ tema_6/app/src/main/java/es/codigonline/proyecto/tema_6/MainActivity.kt
@@ -2,10 +2,13 @@ package es.codigonline.proyecto.tema_6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import es.codigonline.proyecto.tema_6.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- tema_6/app/src/main/java/es/codigonline/proyecto/tema_6/UsuarioFragment.kt
+++ tema_6/app/src/main/java/es/codigonline/proyecto/tema_6/UsuarioFragment.kt
@@ -5,15 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import es.codigonline.proyecto.tema_6.databinding.FragmentUsuarioBinding
 
 
 class UsuarioFragment : Fragment() {
 
+    private val binding get() = _binding!!
+    private var _binding: FragmentUsuarioBinding? = null
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_usuario, container, false)
+        _binding = FragmentUsuarioBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }

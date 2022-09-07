d7172ecb4d658caee6ad0df74358d8a0df831ef5
Alvaro Ortega Marmol <info@codigonline.com>
2022-09-07 19:07:19 +0200

    NavHost

---
+++ tema_6/app/src/main/java/es/codigonline/proyecto/tema_6/navegacion/NavegacionActivity.kt
@@ -0,0 +1,12 @@
+package es.codigonline.proyecto.tema_6.navegacion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import es.codigonline.proyecto.tema_6.R
+
+class NavegacionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navegacion)
+    }
+}

2c5ee1f4b38cb2acd2524cea18b2d388c7d5e781
Alvaro Ortega Marmol <info@codigonline.com>
2022-09-07 20:20:11 +0200

    BottomNavigationActivity

---
+++ tema_6/app/src/main/java/es/codigonline/proyecto/tema_6/bottom/BottomActivity.kt
@@ -0,0 +1,35 @@
+package es.codigonline.proyecto.tema_6.bottom
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import es.codigonline.proyecto.tema_6.R
+import es.codigonline.proyecto.tema_6.databinding.ActivityBottomBinding
+
+class BottomActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBottomBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityBottomBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_bottom)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
+    }
+}

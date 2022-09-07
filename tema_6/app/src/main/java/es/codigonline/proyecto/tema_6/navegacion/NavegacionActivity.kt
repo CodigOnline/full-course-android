af278e255c7eb3c075d4eb45823cc5604271bb0e
Alvaro Ortega Marmol <info@codigonline.com>
2022-09-07 19:42:35 +0200

    navController

--- tema_6/app/src/main/java/es/codigonline/proyecto/tema_6/navegacion/NavegacionActivity.kt
+++ tema_6/app/src/main/java/es/codigonline/proyecto/tema_6/navegacion/NavegacionActivity.kt
@@ -2,11 +2,21 @@ package es.codigonline.proyecto.tema_6.navegacion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import es.codigonline.proyecto.tema_6.R
+import es.codigonline.proyecto.tema_6.databinding.ActivityNavegacionBinding
 
 class NavegacionActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNavegacionBinding
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_navegacion)
+        binding = ActivityNavegacionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.navhost) as NavHostFragment
+        navController = navHost.navController
     }
 }

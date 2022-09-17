14612c4e90f05535c774505c3cfc1d90677dd395
Alvaro Ortega Marmol <info@codigonline.com>
2022-09-17 12:12:31 +0200

    RecyclerView titles

--- tema_7/app/src/main/java/es/codigonline/proyecto/tema_7/rv_title/RvTitleAdapter.kt
+++ tema_7/app/src/main/java/es/codigonline/proyecto/tema_7/rv_title/RvTitleAdapter.kt
@@ -1,16 +1,29 @@
 package es.codigonline.proyecto.tema_7.rv_title
 
+import android.provider.ContactsContract.Data
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import es.codigonline.proyecto.tema_7.databinding.ItemProductoBinding
 import es.codigonline.proyecto.tema_7.databinding.TitleProductoBinding
 
+
 class RvTitleAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val ITEM_VIEW_TYPE_TITLE = 0
+    private val ITEM_VIEW_TYPE_PRODUCTO = 1
+
+    sealed class DataItem {
+        data class ProductoItem(val producto: Producto) : DataItem() {}
+        data class ProductoTitle(val title: String) : DataItem() {}
+    }
+
+    var items = mutableListOf<DataItem>()
 
     class TitleViewHolder private constructor(val binding: TitleProductoBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(title: String) {}
+        fun bind(title: String) {
+            binding.textView.text = title
+        }
 
         companion object {
             fun newInstance(parent: ViewGroup): TitleViewHolder {
@@ -23,7 +36,10 @@ class RvTitleAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     class ProductoViewHolder private constructor(val binding: ItemProductoBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(title: String) {}
+        fun bind(producto: Producto) {
+            binding.id.text = producto.id.toString()
+            binding.name.text = producto.nombre
+        }
 
         companion object {
             fun newInstance(parent: ViewGroup): ProductoViewHolder {
@@ -32,5 +48,36 @@ class RvTitleAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
                 return ProductoViewHolder(binding)
             }
         }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (items[position]) {
+            is DataItem.ProductoTitle -> ITEM_VIEW_TYPE_TITLE
+            is DataItem.ProductoItem -> ITEM_VIEW_TYPE_PRODUCTO
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            ITEM_VIEW_TYPE_TITLE -> TitleViewHolder.newInstance(parent)
+            ITEM_VIEW_TYPE_PRODUCTO -> ProductoViewHolder.newInstance(parent)
+            else -> throw java.lang.ClassCastException("No se ha reconocido el tipo $viewType")
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is TitleViewHolder -> {
+                holder.bind((items[position] as DataItem.ProductoTitle).title)
+            }
+            is ProductoViewHolder -> {
+                holder.bind((items[position] as DataItem.ProductoItem).producto)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
     }
 }
